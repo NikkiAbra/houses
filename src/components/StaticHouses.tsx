234434203df7fb3d714e2ef85b6c91c459d21dd6
NikkiAbra <115661560+NikkiAbra@users.noreply.github.com>
2026-04-10 18:18:10 +0300

    static houses update

--- src/components/StaticHouses.tsx
+++ src/components/StaticHouses.tsx
@@ -2,24 +2,30 @@ import { useMemo } from 'react'
 import { useGLTF, useTexture } from '@react-three/drei'
 import * as THREE from 'three'
 
-import housesUrl from '../../house_models/static/houses_to_web.glb?url'
-import texUrl    from '../../textures/static_houses/static_texture.jpg?url'
+import glassLeftUrl  from '../../house_models/static/static_glass/glass_house_left.glb?url'
+import glassRightUrl from '../../house_models/static/static_glass/glass_house_right.glb?url'
+import plasticUrl    from '../../house_models/static/static_plastic/plastic_houses.glb?url'
 
-export function StaticHouses() {
-  const { scene } = useGLTF(housesUrl)
+import texGlassLeftUrl  from '../../textures/static_houses/glass/projected_diff_left.jpg?url'
+import texGlassRightUrl from '../../textures/static_houses/glass/projected_diff_right.jpg?url'
+import texPlasticUrl    from '../../textures/static_houses/plastic/plastic_houses_diff.jpg?url'
+
+useGLTF.preload(glassLeftUrl)
+useGLTF.preload(glassRightUrl)
+useGLTF.preload(plasticUrl)
+
+function StaticMesh({ url, texUrl }: { url: string; texUrl: string }) {
+  const { scene } = useGLTF(url)
   const texture   = useTexture(texUrl)
 
   const clonedScene = useMemo(() => {
-    texture.flipY     = false
+    texture.flipY      = false
     texture.needsUpdate = true
 
-    const mat = new THREE.MeshBasicMaterial({ map: texture })
-
+    const mat   = new THREE.MeshBasicMaterial({ map: texture })
     const clone = scene.clone(true)
     clone.traverse((obj) => {
-      if (obj instanceof THREE.Mesh) {
-        obj.material = mat
-      }
+      if (obj instanceof THREE.Mesh) obj.material = mat
     })
     return clone
   }, [scene, texture])
@@ -27,4 +33,12 @@ export function StaticHouses() {
   return <primitive object={clonedScene} />
 }
 
-useGLTF.preload(housesUrl)
+export function StaticHouses() {
+  return (
+    <>
+      <StaticMesh url={glassLeftUrl}  texUrl={texGlassLeftUrl}  />
+      <StaticMesh url={glassRightUrl} texUrl={texGlassRightUrl} />
+      <StaticMesh url={plasticUrl}    texUrl={texPlasticUrl}    />
+    </>
+  )
+}
